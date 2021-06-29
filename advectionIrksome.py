1dd1b8f3bc1107f8af4ae9f38b5873204007aabe
Nikola Wullenweber <ga74maq@mytum.de>
2021-06-29 14:45:57 +0200

    code adjustments

--- advectionIrksome.py
+++ advectionIrksome.py
@@ -1,19 +1,14 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Jun  7 13:22:31 2021
-
-@author: nikol
-"""
 
 from fenics import * 
 
-from irksome import GaussLegendre, RadauIIA, Dt
+from irksome import GaussLegendre, RadauIIA, Dt, getForm
 
 import numpy as np
 
 
 
-
+bt=  RadauIIA(1)
+print(bt.A)
 nx = ny = 8
 
 msh = IntervalMesh(10, 0, 1)
@@ -31,33 +26,25 @@ E=V.ufl_element()*V.ufl_element()
 Vbig= FunctionSpace(V.mesh(),E)
 c=Constant(2.0)
 u_D = Expression('x[0]-c*t', degree=1, c=c,t=0)
+u_n = interpolate(u_D, V)
         
+
+u=TrialFunction(V)
+v = TestFunction(V)
+F_n = F = u*v*dx + u_n*v*dx-c*dt*Dt(u)*v*dx
+
 def boundary(x, on_boundary):
             return on_boundary
         
-bc = []
-for i in range(2):
-    bc.append(DirichletBC(Vbig.sub(i), u_D, boundary))
-u_n = interpolate(u_D, V)
-
-
-(sigma, u) = TrialFunctions(Vbig)
-
-k=Function(Vbig)
-k0,k1=split(k)
-v0, v1 = TestFunctions(Vbig)
-u0= u_n + dt*Constant(0.5)*k0+dt*Constant(0.5)*k1
-u1 = u0 + dt*Constant(0.5)*k0+dt*Constant(0.5)*k1     
-F = sigma*v0*dx + u*v1*dx + u0*dx + u1*dx - dt*Dx(u0,0)*v0*k0*dx - dt*Dx(u1,0)*v1*k1*dx
-a, L = lhs(F), rhs(F)
-w=Function(Vbig)
+bc = DirichletBC(V, u_D, boundary)
+F, bcs = getForm(F_n, bt, V, dt, bc)
 
 # Residual
 r = u - u_n + dt * c * Dx((u+u_n)/2,0)
 # Add SUPG stabilisation terms (from https://fenicsproject.org/qa/13458/how-implement-supg-properly-advection-dominated-equation/)
 h = 0.1  # should be interval size. Don't know how to extract this from msh right now.
 tau = h/(2.0*c) # tau from SUPG fenics example
-F += tau * c * Dx(v0,0) * r * dx
+F += tau * c * Dx(v,0) * r * dx
 a, L = lhs(F), rhs(F)  
 
 u = Function(V)
@@ -71,15 +58,15 @@ for n in range(num_steps):
     u_D.t = t
 
     # Compute solution
-    solve(a == L, w, bc)
+    solve(a == L, u, bc)
 
     # Plot solution
     #plot(u)
 
     # Compute error at vertices
     u_e = interpolate(u_D, V)
-    print(w.vector().get_local())
-    arrayY.append(w.vector().get_local().max())
+    print(u.vector().get_local())
+    arrayY.append(u.vector().get_local().max())
     arrayX.append(t)
 
     # Update previous solution
